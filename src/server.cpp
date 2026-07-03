ffcf02fd1aac1b1eb71bb1399301d6c16dafdc4e
inevitableflash <divykphman26@gmail.com>
2026-07-04 01:49:29 +0530

    Fixing server.cpp code

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,7 @@
 #include "server.h"
 #include "resp.h"
+#include <algorithm>
+#include <sstream>
 
 RedisServer::RedisServer(int port) {
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -34,18 +36,22 @@ void RedisServer::start() {
   running = true;
   std::cout << "Server started, waiting for connections..." << std::endl;
 
-  int client_socket =
-      accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-  if (client_socket < 0) {
-    perror("accept failed");
-    stop();
-    exit(EXIT_FAILURE);
-  }
-  std::cout << "Connection accepted" << std::endl;
-  // TODO: Keep server running
-  // handleClient(client_socket);
   while (running) {
-    handleClient(client_socket);
+    int client_socket =
+        accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    if (client_socket < 0) {
+      perror("accept failed");
+      continue;
+    }
+    std::cout << "Connection accepted" << std::endl;
+
+    bool clientConnected = true;
+    while (running && clientConnected) {
+      clientConnected = false;
+      handleClient(client_socket, clientConnected);
+    }
+    close(client_socket);
+    std::cout << "Connection closed, waiting for next client..." << std::endl;
   }
   stop();
 }
@@ -58,19 +64,38 @@ void RedisServer::stop() {
   }
 }
 
-void RedisServer::handleClient(int client_socket) {
+void RedisServer::handleClient(int client_socket, bool &keepGoing) {
   char buffer[MAX_MESSAGE_LEN] = {0};
-  read(client_socket, buffer, MAX_MESSAGE_LEN);
-  std::cout << "Received: " << buffer << std::endl;
+  int bytesRead = read(client_socket, buffer, MAX_MESSAGE_LEN);
 
-  std::stringstream inputStream(buffer);
-  Resp resp(inputStream);
-  Value command = resp.Read();
+  if (bytesRead <= 0) {
+    std::cout << "Client disconnected" << std::endl;
+    return;
+  }
 
-  // TODO: Handle command in Redis simulated database:::::::
-  // Need command sanitizer/executor for this as well as an in-memory database
-  // simulation
-  std::string response = "+OK\r\n";
+  std::string response;
+  try {
+    std::stringstream inputStream(std::string(buffer, bytesRead));
+    Resp resp(inputStream);
+    Value command = resp.Read();
+
+    if (command.array.empty()) {
+      throw std::runtime_error("Empty command");
+    }
+
+    std::string commandName = command.array[0].bulk;
+    std::transform(commandName.begin(), commandName.end(),
+                    commandName.begin(), ::toupper);
+
+    std::vector<Value> args(command.array.begin() + 1, command.array.end());
+
+    Value result = store.executeCommand(commandName, args);
+    response = result.translateToResp();
+  } catch (const std::exception &e) {
+    response = std::string("-ERR ") + e.what() + "\r\n";
+  }
+
   send(client_socket, response.c_str(), response.length(), 0);
   std::cout << "Response sent " << response << std::endl;
+  keepGoing = true;
 }
